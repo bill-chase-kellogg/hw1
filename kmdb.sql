ffd1b1451e77f48dc38642b56a1a68a89e96ca4d
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-01-26 00:45:02 +0000

    Movies Table created

--- kmdb.sql
+++ kmdb.sql
@@ -67,14 +67,37 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  year_released TEXT,
+  mpaa_rating TEXT,
+  director TEXT
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  );
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO movies (id ,name, year_released, mpaa_rating, director)
+VALUES (1, "Batman Begins", "2005", "PG-13","Christopher Nolan")
+;
+INSERT INTO movies (id ,name, year_released, mpaa_rating, director)
+VALUES (2, "The Dark Knight", "2008", "PG-13","Christopher Nolan")
+;
+INSERT INTO movies (id ,name, year_released, mpaa_rating, director)
+VALUES (3, "The Dark Knight Rises", "2012", "PG-13","Christopher Nolan")
+;
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -82,7 +105,10 @@
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+SELECT name, year_released,mpaa_rating,director
+FROM movies
+WHERE director = "Christopher Nolan"
+;
 
 -- Prints a header for the cast output
 .print ""
